deaade0147bfc085109bf35199144538196b9a3b
yeshouyi-pd <172793208@qq.com>
2020-10-27 09:17:21 +0800

    添加gateway 模块

--- system/src/main/java/com/pd/wx/system/SystemApplication.java
+++ system/src/main/java/com/pd/wx/system/SystemApplication.java
@@ -6,10 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class SystemApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
